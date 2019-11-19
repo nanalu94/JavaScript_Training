6cf2bcbb57a5526babccf321d31826baeb38981f
Adri L <46640550+nanalu94@users.noreply.github.com>
2019-11-18 20:23:08 -0700

    Update celsius2fahrenheit.js

--- celsius2fahrenheit.js
+++ celsius2fahrenheit.js
@@ -1,4 +1,5 @@
-const CelsiusTemp=Celsius;
-const Celsius2Fahrenheit = (CelsiusTemp - 32) / 1.8;
+const CelsiusTemp=Celsius{
+            let Celsius2Fahrenheit = (CelsiusTemp - 32) / 1.8;
+}
 console.log(CelsiusTemp 'in Celsius is' Celsius2fahrenheit);
             
